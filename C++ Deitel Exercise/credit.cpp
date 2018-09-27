fda4e130bcc25c7782b941c54e010698666a29d7
Cagri Esen <32637622+esencgr@users.noreply.github.com>
2018-09-27 18:30:16 +0300

    style edit

--- C++ Deitel Exercise/credit.cpp
+++ C++ Deitel Exercise/credit.cpp
@@ -7,28 +7,28 @@ float ilk_bakiye,harcama,kredi,kredi_limit,son_bakiye;
 	 
 	while (1){
 		
-	    cout<< "hesap no(cikis -1):";
-		cin >> hesap;
+	  cout<< "hesap no(cikis -1):";
+	  cin >> hesap;
 
-		    if(hesap == -1)
-		    break;
+		if(hesap == -1)
+		break;
 		
 		cout << "ilk bakiye:";
 		cin  >> ilk_bakiye;    
 		cout << "toplam harcama:";
 		cin  >> harcama;		
-	    cout << "toplam kredi:";
+	        cout << "toplam kredi:";
 		cin  >> kredi;
-	    cout << "kredi limiti:";
+	        cout << "kredi limiti:";
 		cin  >> kredi_limit;
 		cout <<"----------------------------------"<<endl;
 		son_bakiye = ilk_bakiye + harcama-kredi;
-	    cout << "hesap no:"<< hesap;
-	    cout << endl;
-	    cout << "kredi limiti:"<< (float)kredi_limit;
-	    cout << endl;
+	        cout << "hesap no:"<< hesap;
+	        cout << endl;
+	        cout << "kredi limiti:"<< (float)kredi_limit;
+	        cout << endl;
 		cout << "son_bakiye:"<< (float)son_bakiye;
-	    cout << endl;
+	        cout << endl;
 	    
 		if (kredi_limit < son_bakiye)
 		   cout <<"kredi limiti asildi." <<endl;
@@ -36,9 +36,10 @@ float ilk_bakiye,harcama,kredi,kredi_limit,son_bakiye;
 		   cout <<"yapilabilecek harcama tutari:" <<(float)kredi_limit-son_bakiye<<endl;
 		   
 		cout <<"---------------------------------"<<endl ;
-	    cout <<endl;
+	        cout <<endl;
 		
 	}
+}
 
- }
+i
 
