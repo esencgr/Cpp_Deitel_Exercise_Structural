77115c6fa32d92c86d65b0ed881bbd0243908f62
Cagri Esen <esencgr@gmail.com>
2018-09-29 00:34:41 +0300

    style edit

--- C++ Deitel Exercise/triangle_complex.cpp
+++ C++ Deitel Exercise/triangle_complex.cpp
@@ -10,36 +10,32 @@ first = hipo;
     while (counter < 2){
         cin >> side;
         if (side > hipo){
-		     middle = hipo;
-			 hipo = side;    
-		}
-		else if (side > middle) 
-			 middle = side;
+		 middle = hipo;
+		 hipo = side;    
+        }
+	else if (side > middle) 
+	         middle = side;
 			 
         if (first != hipo  &&  first != middle)
-		     small = first;
+		  small = first;
 		    
-		else if(side <= middle)
-		     small = side;
+	else if(side <= middle)
+		  small = side;
 	counter++;
-	}   
+     }   
 cout << " hipo:"<< hipo << endl;
 cout << " middle:"<< middle << endl ; 
 cout << " small:"<< small << endl;
-  /*
-            if (hipo*hipo== middle*middle + small*small)
-            	
-            	cout << "The three numbers could be sides of a right triangle" << endl;
-			
-			else
-                cout << "The three numbers probably are not the sides of a right triangle" << endl;
-  
-       
-            cout << "hipo:"<< hipo ;*/
-            
-            return 0;
-    
 
+        if (hipo*hipo== middle*middle + small*small)
+            	
+            cout << "The three numbers could be sides of a right triangle" << endl;
+			
+	else
+            cout << "The three numbers probably are not the sides of a right triangle" << endl;
+  
+            cout << "hipo:"<< hipo ;
             
+            return 0;       
  }
 
