d5e62b8ccbbdd9ed628df1a7c90d0d7a6002ebd4
Cagri Esen <esencgr@gmail.com>
2019-01-04 15:24:55 +0300

    modified

--- calendar.cpp
+++ calendar.cpp
@@ -3,135 +3,134 @@
 using namespace std;
 int main(){
 int year, month, day;
-	
+
 cout << "enter year (-1 to exit):";
 cin >> year;
-    
+
 	while(year != -1){
 
    	  switch (year){
-   	  	      
+
    	    case 1995:
 
    	  	    for (month = 1; month <= 12; month++){
-   	  	    	
+
    	  	    	if  (month == 1)
    	  	    	    cout << "JANUARY"<< endl;
    	  	    	else if  (month == 2)
-   	  	    	    cout << "FEBRUARY"<< endl; 
+   	  	    	    cout << "FEBRUARY"<< endl;
 				else if  (month == 3)
    	  	    	    cout << "MARCH"<< endl;
 			    else if  (month == 4)
-   	  	    	    cout << "APRÝL "<< endl; 
+   	  	    	    cout << "APRï¿½L "<< endl;
 				else if  (month == 5)
-   	  	    	    cout << "MAY"<< endl;		 
+   	  	    	    cout << "MAY"<< endl;
 			    else if  (month == 6)
-   	  	    	    cout << "JUNE"<< endl; 
+   	  	    	    cout << "JUNE"<< endl;
 				else if  (month == 7)
-   	  	    	    cout << "JULY"<< endl;	
+   	  	    	    cout << "JULY"<< endl;
 			    else if  (month == 8)
-   	  	    	    cout << "AUGUST"<< endl;		 
+   	  	    	    cout << "AUGUST"<< endl;
 				else if  (month == 9)
    	  	    	    cout << "SEPTEMBER"<< endl;
 				else if  (month == 10)
-   	  	    	    cout << "OCTOBER"<< endl;	
+   	  	    	    cout << "OCTOBER"<< endl;
 			    else if  (month == 11)
-   	  	    	    cout << "NOVEMBER"<< endl;		 
+   	  	    	    cout << "NOVEMBER"<< endl;
 				else if  (month == 12)
-   	  	    	    cout << "DECEMBER"<< endl;	
-						 	 		 	    
+   	  	    	    cout << "DECEMBER"<< endl;
+
    	  	        if (month % 2 == 0){
-						 
+
 				for(day = 1; day <= 30; day++){
-					 	
+
 				        if (month == 2 && day == 29)
 					break;
-					 	
+
 					cout << "\t" <<day;
 					if (day % 7 == 0)
 					cout << endl;
 			        }
-			
+
 			}
-			
-                        else 
+
+                        else
 				for(day = 1; day <= 31; day++){
-					 	 
+
 					cout << "\t" <<day;
 					if (day % 7 == 0)
 					cout << endl;
 			}
    	  	        cout << endl;
-				cout << "--------------------------------------------------------------"<< endl;	   
-		    } 
-				  
-   	  	break;  
-   	  	    
+				cout << "--------------------------------------------------------------"<< endl;
+		    }
+
+   	  	break;
+
    	  	case 1996:
-	  	      	
+
    	  	    for (month = 1; month <= 12; month++){
-   	  	    	
+
    	  	    	if  (month == 1)
    	  	    	    cout << "JANUARY"<< endl;
    	  	    	else if  (month == 2)
-   	  	    	    cout << "FEBRUARY"<< endl; 
+   	  	    	    cout << "FEBRUARY"<< endl;
 				else if  (month == 3)
    	  	    	    cout << "MARCH"<< endl;
 			    else if  (month == 4)
-   	  	    	    cout << "APRÝL "<< endl; 
+   	  	    	    cout << "APRï¿½L "<< endl;
 				else if  (month == 5)
-   	  	    	    cout << "MAY"<< endl;		 
+   	  	    	    cout << "MAY"<< endl;
 			    else if  (month == 6)
-   	  	    	    cout << "JUNE"<< endl; 
+   	  	    	    cout << "JUNE"<< endl;
 				else if  (month == 7)
-   	  	    	    cout << "JULY"<< endl;	
+   	  	    	    cout << "JULY"<< endl;
 			    else if  (month == 8)
-   	  	    	    cout << "AUGUST"<< endl;		 
+   	  	    	    cout << "AUGUST"<< endl;
 				else if  (month == 9)
    	  	    	    cout << "SEPTEMBER"<< endl;
 				else if  (month == 10)
-   	  	    	    cout << "OCTOBER"<< endl;	
+   	  	    	    cout << "OCTOBER"<< endl;
 			    else if  (month == 11)
-   	  	    	    cout << "NOVEMBER"<< endl;		 
+   	  	    	    cout << "NOVEMBER"<< endl;
 				else if  (month == 12)
-   	  	    	    cout << "DECEMBER"<< endl;	
-						 	 		 	    
+   	  	    	    cout << "DECEMBER"<< endl;
+
    	  	        if (month % 2 == 0){
-						 
+
 					for(day = 1; day <= 30; day++){
-					 	
+
 					 	if (month == 2 && day == 30)
 					 	break;
-					 	
+
 					 	cout << "\t" <<day;
 					 	if (day % 7 == 0)
 					 	cout << endl;
 					 }
-			
+
 			}
-			
-                        else 
+
+                        else
 				    for(day = 1; day <= 31; day++){
-					 	 
+
 					 	cout << "\t" <<day;
 					 	if (day % 7 == 0)
 					 	cout << endl;
 				    }
    	  	        cout << endl;
-			cout << "--------------------------------------------------------------"<< endl;	   
-		   } 
-				  
+			cout << "--------------------------------------------------------------"<< endl;
+		   }
+
    	  	break;
 
-   	  	
+
    	  	default:
    	  		cout << " invalid year ";
    	  		break;
-   	  		
+
          }
 	 cout << endl;
          cout << "enter year  (-1 to exit):";
          cin >> year;
         }
     }
-
