8405e5d9d7e4690554927fa148054248ced3e494
Cagri Esen <esencgr@gmail.com>
2019-01-05 12:55:52 +0300

    delete file

--- sales.cpp
+++ sales.cpp
@@ -2,43 +2,43 @@
 #include<math.h>
 using namespace std;
 int main(){
-	
+
 int no,quantity;
 double total=0.0;
-	
+
 cout<< "enter product code (exit(-1)):";
 cin >>no;
-	
+
      while (no!=-1){
 	cout<<"quantity:";
 	cin >> quantity;
-		
+
 	   switch (no){
-		case 1: 
+		case 1:
 		    total+=quantity*2.98;
 		break;
-		case 2: 
+		case 2:
 		    total+=quantity*4.50;
-		break;	
-		case 3: 
+		break;
+		case 3:
 		    total+=quantity*9.98;
 		break;
-		case 4: 
+		case 4:
 		    total+=quantity*4.49;
-		break;	
-		case 5: 
+		break;
+		case 5:
 		    total+=quantity*6.87;
-		break;	
+		break;
 		default:
 		    cout << "invalid product code!!"<<endl;
 	        break;
 		}
-		
+
 			cout<< "enter product code (exit(-1)):";
-	        cin >>no;
-	
+	    cin >>no;
+
 	}
 cout<< "total:"<<(double)total<<endl;
-	
+
 return 0;
 }
--- small_number.cpp
+++ small_number.cpp
@@ -2,21 +2,20 @@
 #include<math.h>
 using namespace std;
 int main(){
-	
+
 int value,piece,small;
-	
+
 cout << "piece of integers:";
 cin >> piece;
 cout << "enter integers:";
 cin >> small;
-	
-        for(int i = 0;   i<piece-1;   i++){
-	
+
+  for(int i = 0;   i<piece-1;   i++){
+
 	   cin >> value;
-	
 	   if(value < small)
-	   small = value;	
-	    
+	   small = value;
+
 	}
-cout << "smallest number :" << small;	   
+cout << "smallest number :" << small;
 }
