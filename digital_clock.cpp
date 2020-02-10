ed8a1909f9d44a1e81ee83db8a9a49b8b1997fe2
Cagri Esen <esencgr@gmail.com>
2020-02-10 11:59:14 +0300

    add

--- digital_clock.cpp
+++ digital_clock.cpp
@@ -8,21 +8,21 @@ int main(){
     int second=0;
     int minute=0;
     int hour=0;
-    
+
   while (1){
         cout << "digital clock ---> ";
         cout << hour/3600 << ":"<< minute/60 << ":" << second ;
-   
+
           if (second == 59)
             second = -1;
           if (minute == 3599)
             minute = -1;
-          
+
         second++;
         minute++;
         hour++;
-        
-  sleep (1);   
+
+  sleep (1);
   system("cls");
   }
 }
